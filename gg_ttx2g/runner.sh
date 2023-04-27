b0bdd3209632126275f789bb1446390a9e3d5e57
Zenny Wettersten <zenny.wettersten@cern.ch>
2023-04-27 19:43:34 +0200

    maybe this works

--- gg_ttx2g/runner.sh
+++ gg_ttx2g/runner.sh
@@ -1,9 +1,1 @@
-while getopts lhe:rwgt:out: flag
-do
-    case "${flag}" in
-        lhe) lhePt=${OPTARG};;
-        rwgt) rePt=${OPTARG};;
-        out) outputPt=${OPTARG};;
-    esac
-done
-./SubProcesses/P1_Sigma_sm_gg_ttxgg/runner.exe "-lhe=$lhePt -rwgt=$rePt -out=$outputPt"; 
+./SubProcesses/P1_Sigma_sm_gg_ttxgg/runner.exe $1 $2 $3;
